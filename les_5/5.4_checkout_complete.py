91c2a1e9dbd20787863e0456f57df8acaeaeaeba
Mr-Zeno <62742588+Mr-Zeno@users.noreply.github.com>
2022-02-22 09:36:01 +0100

    some changes

--- les_5/5.4_checkout_complete.py
+++ les_5/5.4_checkout_complete.py
@@ -1,5 +1,6 @@
 '''kopieer je code van 5.3'''
 '''en zorg dat de title op de lichtkrant verschijnt'''
+'''clk = "P9", din = "P10", cs = "P19" '''
 
 import lichtkrant as lk
 lk.print_on_matrix("test")
